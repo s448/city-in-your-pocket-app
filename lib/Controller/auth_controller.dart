35f6def81cd337a8fec8d7bc612048b0a26b4b35
s448 <elsaedmohassan@gmail.com>
2023-09-22 20:29:14 +0300

    fix bugs

--- lib/Controller/auth_controller.dart
+++ lib/Controller/auth_controller.dart
@@ -5,7 +5,6 @@ import 'package:cityinpocket/Services/fcm_services.dart';
 import 'package:cityinpocket/routes.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
